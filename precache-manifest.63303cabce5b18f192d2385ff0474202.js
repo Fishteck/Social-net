e12b8121d3b68dd95f7b825a9b05697fcd7ba1ec
Roman <roman.makuha@gmail.com>
2019-09-15 17:35:23 +0300

    Updates

---
+++ precache-manifest.63303cabce5b18f192d2385ff0474202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164c91f27fa6ee6d391e71f9ae5c535e",
+    "url": "/Social-net/index.html"
+  },
+  {
+    "revision": "a2893ee9d47975a164b3",
+    "url": "/Social-net/static/css/main.f9be7bf7.chunk.css"
+  },
+  {
+    "revision": "9ab311bdb1347f7c1a98",
+    "url": "/Social-net/static/js/2.a0f39c1a.chunk.js"
+  },
+  {
+    "revision": "a2893ee9d47975a164b3",
+    "url": "/Social-net/static/js/main.170a0f41.chunk.js"
+  },
+  {
+    "revision": "a4ba8be98b443c0dd83d",
+    "url": "/Social-net/static/js/runtime~main.461b4bf1.js"
+  },
+  {
+    "revision": "fc047347b17f7df7ff288d78c8c281cf",
+    "url": "/Social-net/static/media/avatar.fc047347.png"
+  },
+  {
+    "revision": "126d0665ffff21d9df0f5a433d11bb40",
+    "url": "/Social-net/static/media/preloader.126d0665.svg"
+  }
+]);
